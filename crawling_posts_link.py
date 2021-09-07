c5bdc34c31c08b290cde3c0f5480562524e3f889
nhat <nhatran241>
2021-09-08 02:06:51 +0700

    oke

--- crawling_posts_link.py
+++ crawling_posts_link.py
@@ -6,9 +6,10 @@ sitemaps = pd.read_csv('post.csv')
 with open('post-final.csv', 'a') as f:
     for index,sitemap in sitemaps.iterrows():
         url = sitemap[2]
-        if pd.notna(url) & url.find('.txt') != -1:
-            r = requests.get(url, allow_redirects=True)
-            print(r.content)
-            f.write('\n')
-            df = pd.DataFrame({"title": sitemap[1], "content": r.content}, index=[index])
-            df.to_csv('post-final.csv', index=True, header=False, mode='a')
+        if pd.notna(url):
+            if  ".txt" in url:
+                print(url)
+                r = requests.get(url, allow_redirects=True)
+                f.write('\n')
+                df = pd.DataFrame({"title": sitemap[1], "content": r.content}, index=[index])
+                df.to_csv('post-final.csv', index=True, header=False, mode='a')
